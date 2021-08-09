b30d242f0c4f2afa319743c7baf1d1c965efb803
ELynx <1621251+ELynx@users.noreply.github.com>
2021-08-09 19:26:56 +0300

    replace other mentions as well, no matter how related

--- edu.harvard.i2b2.ontology/etc/scripts/original/insert_data.sql
+++ edu.harvard.i2b2.ontology/etc/scripts/original/insert_data.sql
@@ -2,9 +2,9 @@
 VALUES('rpdr', 'rpdr', 'N', 0, 'RPDR', '\RPDR', 'N', 'CA', '\RPDR', NULL, NULL, NULL, NULL, 'concept_cd', 'concept_dimension', 'concept_path', 'T', 'LIKE', '\RPDR', '20080304 15:10:43', NULL, NULL)
 
 	INSERT INTO [dbo].[ONT_DB_LOOKUP]([c_domain_id], [c_project_path], [c_owner_id], [c_db_fullschema], [c_db_datasource],[c_db_servertype], [c_db_nicename], [c_comment], [c_entry_date], [c_change_date], [c_status_cd])
-VALUES('ra_test', 'Demo/', '@', 'ra_mart_test.dbo', 'java:OntologyDemoDS', 'SQLSERVER', 'RA Mart',  NULL,  
+VALUES('ra_test', 'Demo/', '@', 'ra_mart_test.dbo', 'java:IrisDS', 'SQLSERVER', 'RA Mart',  NULL,  
 '20080304 15:10:43', NULL, NULL)
 
 	INSERT INTO [dbo].[ONT_DB_LOOKUP]([c_domain_id], [c_project_path], [c_owner_id], [c_db_fullschema], [c_db_datasource],[c_db_servertype], [c_db_nicename], [c_comment], [c_entry_date], [c_change_date], [c_status_cd])
-VALUES('ra_test', 'Demo/', 'demo', 'ra_mart_test.dbo', 'java:OntologyDemoDS', 'SQLSERVER', 'RA Mart',  NULL,  
+VALUES('ra_test', 'Demo/', 'demo', 'ra_mart_test.dbo', 'java:IrisDS', 'SQLSERVER', 'RA Mart',  NULL,  
 '20080304 15:10:43', NULL, NULL)
